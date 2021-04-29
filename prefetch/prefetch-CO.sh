6ca24817833d937ee617402fc3edce205ea4d080
Jeff Boody <jeffboody@gmail.com>
2021-04-29 09:44:37 -0600

    rename osmdbv5 cache

--- prefetch/prefetch-CO.sh
+++ prefetch/prefetch-CO.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-LD_PRELOAD=/usr/local/lib/libjemalloc.so unbuffer ./osmdb-prefetch -pf=CO 2.0 cache-CO.sqlite3 planet.sqlite3 | tee log-CO.txt
+LD_PRELOAD=/usr/local/lib/libjemalloc.so unbuffer ./osmdb-prefetch -pf=CO 2.0 osmdbv5-CO.sqlite3 planet.sqlite3 | tee log-CO.txt
--- prefetch/prefetch-WW.sh
+++ prefetch/prefetch-WW.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-LD_PRELOAD=/usr/local/lib/libjemalloc.so unbuffer ./osmdb-prefetch -pf=WW 4.0 cache-WW.sqlite3 planet.sqlite3 | tee log-WW.txt
+LD_PRELOAD=/usr/local/lib/libjemalloc.so unbuffer ./osmdb-prefetch -pf=WW 4.0 osmdbv5-WW.sqlite3 planet.sqlite3 | tee log-WW.txt
