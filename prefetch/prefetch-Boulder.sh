f05fc7ad9a8cdf400a1cc638b0afd94e1e0b535a
Jeff Boody <jeffboody@gmail.com>
2022-08-20 11:02:36 -0600

    release osmdbv9-20220424

--- prefetch/prefetch-Boulder.sh
+++ prefetch/prefetch-Boulder.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-unbuffer ./osmdb/prefetch/osmdb-prefetch -pf=CO 2.0 osmdbv8-Boulder.bfs Boulder.sqlite3 | tee prefetch-Boulder.log
+unbuffer ./osmdb/prefetch/osmdb-prefetch -pf=CO 2.0 osmdbv9-Boulder.bfs Boulder.sqlite3 | tee prefetch-Boulder.log
--- prefetch/prefetch-CO.sh
+++ prefetch/prefetch-CO.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-unbuffer ./osmdb/prefetch/osmdb-prefetch -pf=CO 2.0 osmdbv8-CO.bfs CO.sqlite3 | tee prefetch-CO.log
+unbuffer ./osmdb/prefetch/osmdb-prefetch -pf=CO 2.0 osmdbv9-CO.bfs CO.sqlite3 | tee prefetch-CO.log
